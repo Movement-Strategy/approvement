8235c2e4bfa4a762c815dd220999cf2fe8255622
Clay Branch <clay@movementstrategy.com>
2014-08-12 13:30:45 -0600

    got calendar days working

--- client/views/calendarDay/calendarDay.js
+++ client/views/calendarDay/calendarDay.js
@@ -1,3 +1,47 @@
+
+statusColorMap = {
+	approved : 'green',
+	rejected : 'red',
+	submitted : 'grey',
+	commented : 'orange',
+};
+
+var setApprovalItemInDay = function(day, approvalItem) {
+	var scope = approvalItem.scope;
+	if(!_.has(day['approval_items'],scope)) {
+		day['approval_items'][scope] = [];
+	}
+	var processedItem = {};
+	
+	// convert the status to a label color
+	processedItem['label_color'] = statusColorMap[approvalItem.status];
+	processedItem['label_icon'] = approvalItem.type;
+	day['approval_items'][scope].push(processedItem);
+	
+	return day;
+}
+
+setAllApprovalItemsInDay = function(day, dayIndex, approvalItemsByDay) {
+	if(_.has(approvalItemsByDay, dayIndex)) {
+		day['approval_items'] = {};	
+		_.map(approvalItemsByDay[dayIndex], function(approvalItem) {
+			day = setApprovalItemInDay(day, approvalItem);
+		});	
+	}
+	return day;
+}
+
+setCalendarDays = function() {
+	var approvalItemsByDay = Session.get('approval_items_by_day');
+	var calendarDays = _.map(Session.get('current_days'), function(day, dayIndex){
+		day['day_name'] = day.name;
+		return setAllApprovalItemsInDay(day, dayIndex, approvalItemsByDay);
+	});
+		
+	Session.set('calendar_days', calendarDays);
+}
+
+
 Template['calendarDay'].helpers({
 });
 
--- client/views/contentCalendar/contentCalendar.js
+++ client/views/contentCalendar/contentCalendar.js
@@ -1,42 +1,9 @@
 Template['contentCalendar'].helpers({
 	calendar_days : function() {
-		return [
-			{
-				day_name : 'Monday',
-				approval_items : {
-					external : [
-						{
-							label_color : 'green',
-							label_icon : 'instagram',
-						},
-					],
-					internal : [
-						{
-							label_color : 'orange',
-							label_icon : 'twitter',
-						},
-					],
-				},
-			},
-			{
-				day_name : 'Tuesday',
-			},
-			{
-				day_name : 'Wednesday',
-			},
-			{
-				day_name : 'Thursday',
-			},
-			{
-				day_name : 'Friday',
-			},
-			{
-				day_name : 'Saturday',
-			},
-			{
-				day_name : 'Sunday',
-			},
-		];
+		
+		// maps a status to the color that's going to be displayed
+		calendarDays = Session.get('calendar_days');
+		return calendarDays;
 	} 
 });
 
