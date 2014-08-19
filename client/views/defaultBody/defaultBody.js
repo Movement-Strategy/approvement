0f7d20b55370b0861dc4bd442438a0cf5f2c0454
Clay Branch <clay@movementstrategy.com>
2014-08-19 16:25:20 -0600

    added ability to login

--- client/views/defaultBody/defaultBody.js
+++ client/views/defaultBody/defaultBody.js
@@ -1,6 +1,9 @@
 Template['defaultBody'].helpers({
 	approval_items_are_ready : function() {
 		return Session.get('approval_items_are_ready');
+	},
+	logged_in : function() {
+		return Meteor.userId() != null;
 	}
 });
 
