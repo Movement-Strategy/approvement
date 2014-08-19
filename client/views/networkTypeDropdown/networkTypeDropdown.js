cd8666d401547dd6370dd472ec4e8e1cff5dee1f
Clay Branch <clay@movementstrategy.com>
2014-08-19 13:03:57 -0600

    removed console.log

--- client/views/networkTypeDropdown/networkTypeDropdown.js
+++ client/views/networkTypeDropdown/networkTypeDropdown.js
@@ -1,13 +1,9 @@
 Template['networkTypeDropdown'].helpers({
 	initialize : function() {
 		Meteor.defer(function(){
-			console.log('firing');
 			$('.network-type-dropdown').dropdown();
 		});
 	},
-	network_type_is_chosen : function() {
-		return Session.get('network_type') != null;
-	}
 });
 
 Template['networkTypeDropdown'].events({
