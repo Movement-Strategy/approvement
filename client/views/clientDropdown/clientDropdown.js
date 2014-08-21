a48cadbe8ccdba0720bf91fcd92220e7ad79fd5d
Clay Branch <clay@movementstrategy.com>
2014-08-21 09:35:38 -0600

    got basic structure of dropdown in place

--- client/views/clientDropdown/clientDropdown.js
+++ client/views/clientDropdown/clientDropdown.js
@@ -1,4 +1,9 @@
 Template['clientDropdown'].helpers({
+	initialize : function() {
+		Meteor.defer(function(){
+			$('.client-type-dropdown').dropdown();
+		});
+	},
 });
 
 Template['clientDropdown'].events({
