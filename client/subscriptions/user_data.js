c428774e7d7e45344a00432bc114e9a925f2a695
Clay Branch <clay@movementstrategy.com>
2014-09-04 14:19:08 -0600

    got logging in and out working

--- client/subscriptions/user_data.js
+++ client/subscriptions/user_data.js
@@ -7,22 +7,25 @@ confirmPageIsReady = function() {
 }
 
 Meteor.subscribe('userData', onReady = function(){
-	var users = Meteor.users.find({}, {fields : {profile : 1}}).fetch();
-	var user = users[0];
-	
-	var profile = user.profile;
-	Session.set('user_type', profile.user_type);
-	var clients = _.has(user.profile, 'clients') ? user.profile.clients : ['usa_today'];
-	var clientsByID = Session.get('clients_by_id');
-	clients = clients == 'all' ? _.keys(clientsByID) : clients;
-	Session.set('current_clients', clients);
-	Session.set('user_name', profile.name);
-	Session.set('user_picture', facebookHandler.getPictureURL(profile.facebook_id));
-	if(!Session.get('clients_are_ready')) {
-		if(clientsByID != {}) {
-			Session.set('selected_client_id', clients[0]);
-			setSelectedClient();
-			Session.set('clients_are_ready', true);
+	Deps.autorun(function(){
+		var users = Meteor.users.find({}, {fields : {profile : 1}}).fetch();
+		if(users.length > 0) {
+			var user = users[0];
+			var profile = user.profile;
+			Session.set('user_type', profile.user_type);
+			var clients = _.has(user.profile, 'clients') ? user.profile.clients : ['usa_today'];
+			var clientsByID = Session.get('clients_by_id');
+			clients = clients == 'all' ? _.keys(clientsByID) : clients;
+			Session.set('current_clients', clients);
+			Session.set('user_name', profile.name);
+			Session.set('user_picture', facebookHandler.getPictureURL(profile.facebook_id));
+			if(!Session.get('clients_are_ready')) {
+				if(clientsByID != {}) {
+					Session.set('selected_client_id', clients[0]);
+					setSelectedClient();
+					Session.set('clients_are_ready', true);
+				}
+			}
 		}
-	}
+	});
 });
--- client/views/mainNav/mainNav.js
+++ client/views/mainNav/mainNav.js
@@ -1,6 +1,24 @@
 Template['mainNav'].helpers({
+	show_dropdown : function() {
+		return !Session.get('details_shown');
+	},
+	user_picture : function() {
+		return Session.get('user_picture');
+	},
+	signed_in : function() {
+		return Meteor.userId() != null;
+	},
+	initializeDropdown : function() {
+		Meteor.defer(function(){
+			$('.user-image-container').dropdown();
+		});
+	}
 });
 
 Template['mainNav'].events({
+	'click .sign-out' : function() {
+		Meteor.logout();
+		Session.set('clients_are_ready', false);
+	}
 });
 
