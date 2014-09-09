b1ed7df8fd26d9992a7ac9ca87f151f8af840e44
Clay Branch <clay@movementstrategy.com>
2014-09-09 09:33:24 -0600

    got basic template populating

--- client/views/creationAssets/creationAssets.js
+++ client/views/creationAssets/creationAssets.js
@@ -1,7 +1,40 @@
+var assetMap = {
+	image : {
+		icon : 'photo',
+		display_name : 'Image',
+	},
+	link : {
+		icon : 'external url',
+		display_name : 'Link',
+	},
+};
+
 Template['creationAssets'].helpers({
 	width_class : function() {
 		return getWidthClass();
 	},
+	assets : function() {
+		var assets = [
+			{
+				id : '1000',
+				url : 'www.google.com',
+				type : 'image',
+			},
+			{
+				id : '2000',
+				url : 'www.espn.com',
+				type : 'link',
+			},
+		];
+		
+		assets = _.map(assets, function(asset){
+			asset['icon'] = assetMap[asset['type']]['icon'];
+			asset['display_name'] = assetMap[asset['type']]['display_name'];
+			return asset;
+		});
+		
+		return assets;
+	}
 });
 
 Template['creationAssets'].events({
