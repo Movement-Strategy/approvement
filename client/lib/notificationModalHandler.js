cef85f7b8ff1760353cff5763e54faa49feffc33
Clay Branch <clay@movementstrategy.com>
2014-09-30 09:05:50 -0600

    modal no longer shows by default

--- client/lib/notificationModalHandler.js
+++ client/lib/notificationModalHandler.js
@@ -1,7 +1,7 @@
 notificationModalHandler = {
 	initializeModal : function() {
 		Meteor.defer(function(){
-			$('.notification-modal').modal('show');
+			$('.notification-modal').modal();
 		});
 	}
 };
