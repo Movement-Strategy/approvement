3910b9c8d1c78d30ceb8d6fd8732fe735bb1e68b
Clay Branch <clay@movementstrategy.com>
2014-10-03 14:54:48 -0600

    removed all gif files

--- client/lib/gifHandler.js
+++ client/lib/gifHandler.js
@@ -34,21 +34,21 @@ gifHandler = {
 		}
 	},
 	getGifURL : function(){
-		var gif = this.gifs[Math.floor(Math.random() * this.gifs.length)];
-		return 'images/' + gif;
+		return this.gifs[Math.floor(Math.random() * this.gifs.length)];
 	},
 	gifs : [
-		'gif1.gif',
-		'gif2.gif',
-		'gif3.gif',
-		'topgun.gif',
-		'elvis.gif',
-		'fistpump.gif',
-		'never.gif',
-		'wow.gif',
-		'dog-five.gif',
-		'kingpin.gif',
-		'yes.gif',
-		'slowclap.gif',
+		'http://media.giphy.com/media/tyxovVLbfZdok/giphy.gif',
+		'http://media1.giphy.com/media/JQNM4AgN7lFUA/giphy.gif',
+		'http://media.giphy.com/media/5xtDarvV3mbQpRABhNC/giphy.gif',
+		'http://media.giphy.com/media/112VS6QqBw52Ks/giphy.gif',
+		'http://media.giphy.com/media/dkGhBWE3SyzXW/giphy.gif',
+		'http://media.giphy.com/media/iFmxR5QdkEQKI/giphy.gif',
+		'http://media.giphy.com/media/kKo2x2QSWMNfW/giphy.gif',
+		'http://media.giphy.com/media/13wAmg8pGU2olq/giphy.gif',
+		'http://media.giphy.com/media/gbbZL8JOuHWDu/giphy.gif',
+		'http://media.giphy.com/media/RpVZL0sy3vrri/giphy.gif',
+		'http://media.giphy.com/media/5xrkJe3IJKSze/giphy.gif',
+		'http://media.giphy.com/media/13py6c5BSnBkic/giphy.gif',
+		'http://media.giphy.com/media/NsFDCblOHz9QI/giphy.gif',
 	]	
 };
