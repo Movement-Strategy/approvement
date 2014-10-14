39eecb207db7526635c1d1211fe3d232188b51f0
Clay Branch <clay@movementstrategy.com>
2014-10-14 10:41:41 -0600

    image urls can now be copied

--- client/lib/imageUploadHandler.js
+++ client/lib/imageUploadHandler.js
@@ -25,11 +25,19 @@ imageUploadHandler = {
 			var sampleURL = 'http://lorempixel.com/' + heightDetails['width'] + '/' + heightDetails['height'] + '/';
 			currentItemContents = Session.get('current_item_contents');
 			currentURL = _.has(currentItemContents, 'image_url') ? currentItemContents['image_url'] : sampleURL;
+			var itemToCopy = Session.get('item_to_copy');
 			var linkData = Session.get('current_facebook_link_data');
 			var metaTagURL = _.has(linkData , 'og:image') ?  linkData['og:image'] : null;
 			if(metaTagURL && Session.get('uploaded_image_url') == null) {
 				Session.set('uploaded_image_url', metaTagURL);
 			}
+			
+			if(itemToCopy) {
+				if(_.has(itemToCopy.contents, 'image_url')) {
+					Session.set('uploaded_image_url', itemToCopy.contents.image_url);
+				}
+			}
+			
 			return Session.get('uploaded_image_url') == null ? currentURL : Session.get('uploaded_image_url');
 		} else {
 			return null;
