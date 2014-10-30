40969aaac39c2152489bb8e1052ff3fb84e6bca3
Clay Branch <clay@movementstrategy.com>
2014-10-30 09:58:05 -0600

    working through draft board set up

--- client/lib/draftBoardHandler.js
+++ client/lib/draftBoardHandler.js
@@ -1,5 +1,51 @@
 draftBoardHandler = {
+	getColumnMap : function() {
+		return {
+			content_bucket : {
+				display : "Content Bucket",
+			},
+			content : {
+				display : "Content"
+			},
+/*
+			network : {
+				display : "Network"
+			},
+*/
+/*
+			content_type : {
+				display : "Content Type"
+			},
+			day_of_week : {
+				display : "Day of week"
+			},
+			scheduled_time : {
+				display : "Scheduled Time"
+			},
+			reference : {
+				display : "Reference"
+			},
+*/
+		};	
+	},
 	isShown : function() {
 		return Session.get('draft_board_is_shown');	
+	},
+	getColumns : function() {
+		var columns =  _.map(this.getColumnMap(), function(column, columnID){
+			return column;
+		});
+		return columns;
+	},
+	getContentBuckets : function() {
+		return [
+			{
+				columns : [
+					{
+						
+					},
+				],
+			},
+		],
 	},
 };
--- client/views/draftBoard/draftBoard.js
+++ client/views/draftBoard/draftBoard.js
@@ -1,4 +1,17 @@
 Template['draftBoard'].helpers({
+	initializeDropdown : function() {
+		Meteor.defer(function(){
+			$('.network-type-inline').dropdown();
+		});
+	},
+	draft_columns : function() {
+		return draftBoardHandler.getColumns();
+	},
+	initializeTextArea : function() {
+		Meteor.defer(function(){
+			$('textarea.content-input').autosize();
+		});
+	},
 });
 
 Template['draftBoard'].events({
