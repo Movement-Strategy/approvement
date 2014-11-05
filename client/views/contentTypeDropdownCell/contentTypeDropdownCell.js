b19f86e0ebe4c8ace8ef67397a6131c4a65021f8
Clay Branch <clay@movementstrategy.com>
2014-11-05 16:00:26 -0700

    got the dynamic dropdown working completely

--- client/views/contentTypeDropdownCell/contentTypeDropdownCell.js
+++ client/views/contentTypeDropdownCell/contentTypeDropdownCell.js
@@ -9,29 +9,6 @@ Template['contentTypeDropdownCell'].helpers({
 			$('.content-type-dropdown-cell').dropdown('restore default text');
 		});
 	},
-/*
-	text_display : function() {
-		var networkType = contentBucketHandler.getDraftVariableValue('network', this.content_bucket_id);
-		var contentTypes = contentTypeBuilder.getContentTypes(networkType);
-		var that = this;
-		var contentType = _.find(contentTypes, function(contentType){
-			return contentType['value'] == that.value;
-		});
-		console.log(contentType);
-		if(contentType) {
-			return contentType['display'];
-		} else {
-			return 'None';
-		}
-	},
-	output_value : function() {
-		if(_.has(this, 'value')) {
-			return this.value;
-		} else {
-			return this.params.default_value;
-		}
-	},
-*/
 	style_class : function() {
 		return this.params.style_class;
 	},
@@ -40,7 +17,7 @@ Template['contentTypeDropdownCell'].helpers({
 Template['contentTypeDropdownCell'].events({
 	'click .content-type-item' : function(event) {
 		var value = $(event.currentTarget).attr('data-value');
-		$('.content-type-dropdown-cell').dropdown('hide');
+		$('.content-type-dropdown-cell').dropdown('hide').dropdown('set text', $(event.currentTarget).text());		
 		console.log(value);
 	},
 });
