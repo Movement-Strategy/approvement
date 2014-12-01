f0bd9741273617385bac1de9137c8277ff7825a4
Clay Branch <clay@movementstrategy.com>
2014-12-01 14:55:33 -0700

    made arrow keys change date on draft board view

--- client/views/draftBoard/draftBoard.js
+++ client/views/draftBoard/draftBoard.js
@@ -1,4 +1,15 @@
+Template.draftBoard.created = function() {
+    $(document).on('keydown', keyStrokeHandler.handleKeyStrokes);
+};
+
+Template.draftBoard.destroyed = function() {
+    $(document).unbind('keydown');
+};
+
 Template['draftBoard'].helpers({
+	week : function() {
+		return timeHandler.getWeekForSelectedTime();	
+	},
 });
 
 Template['draftBoard'].events({
