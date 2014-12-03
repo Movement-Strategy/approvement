0588917c36e05bfff6e7d8d181792e2b9c5e4939
Clay Branch <clay@movementstrategy.com>
2014-12-03 09:39:13 -0700

    moving stuff around

--- client/views/defaultBody/defaultBody.js
+++ client/views/defaultBody/defaultBody.js
@@ -2,14 +2,11 @@ Template['defaultBody'].helpers({
 	page_is_ready : function() {
 		return pageLoadHandler.pageIsReady();
 	},
+	renderTest : function() {
+		console.log('notification load');	
+	},
 	logged_in : function() {
 		return loginHandler.isLoggedIn();
-	},
-	show_gif : function() {
-		return gifHandler.gifShouldBeShown();
-	},
-	show_draft_board : function() {
-		return draftBoardHandler.isShown();
 	},
 	initializeModal : function() {
 // 		notificationModalHandler.initializeModal();
