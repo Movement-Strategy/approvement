a5d91b57bdc7f62aa9bfbdd14e0f3d7d68e88e96
Clay Branch <clay@movementstrategy.com>
2014-12-08 11:23:17 -0700

    removed ready check in route

--- client/routes/draftBoardWeek.js
+++ client/routes/draftBoardWeek.js
@@ -12,12 +12,10 @@ if(Meteor.isClient) {
 	        	if(loginHandler.isLoggedIn()) {
 		        	var that = this;
 		        	Deps.autorun(function(){
-			        	if(Session.get('draft_items_ready') && Session.get('content_buckets_ready')) {
-				        	calendarBuilder.initializeCalendarWeek(that.params.client, that.params.week);
-				        	Session.set('draft_board_is_shown', true);
-				        	contentBucketHandler.handleContentBuckets();
-				        	draftItemHandler.handleDraftItems();
-			        	}
+			        	calendarBuilder.initializeCalendarWeek(that.params.client, that.params.week);
+			        	Session.set('draft_board_is_shown', true);
+			        	contentBucketHandler.handleContentBuckets();
+			        	draftItemHandler.handleDraftItems();
 		        	});	
 	        	} else {
 		        	Router.go('/login');
