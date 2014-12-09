038543246e0d99d45d984df2b051bc3f9bf1d945
Clay Branch <clay@movementstrategy.com>
2014-12-09 11:02:13 -0700

    removed save button

--- client/views/draftBoardFooter/draftBoardFooter.js
+++ client/views/draftBoardFooter/draftBoardFooter.js
@@ -5,9 +5,6 @@ Template['draftBoardFooter'].events({
 	'click .convert.button' : function() {
 		contentBucketHandler.convertAllDraftItemsToApprovalItems();
 	},
-	'click .save.button' : function() {
-		contentBucketHandler.updateContentBuckets();	
-	},
 	'click .create.button' : function() {
 		contentBucketModalHandler.showModal({}, true);
 	},
