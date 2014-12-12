57449afbd66b6344cd339805db8fe85d910c9ce1
Clay Branch <clay@movementstrategy.com>
2014-12-12 14:03:17 -0700

    working through stuff

--- client/views/imageCell/imageCell.js
+++ client/views/imageCell/imageCell.js
@@ -1,11 +1,15 @@
 Template['imageCell'].helpers({
 	image_url : function() {
-		var imageURL = contentBucketHandler.getValueForDraftVariable('image', this.draft_item_id, this.content_bucket_id);
+// 		var imageURL = contentBucketHandler.getValueForDraftVariable('image', this.draft_item_id, this.content_bucket_id);
+		var imageURL = this.value;
 		if(imageURL == null) {
 			imageURL = '/images/image-place.png';
 		}
 		return imageURL;
 	},
+	show_image : function() {
+		return this.value != null;
+	}
 });
 
 Template['imageCell'].events({
