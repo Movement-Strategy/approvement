30ec32db267dad04790d30caa0350de95e35d6b1
Clay Branch <clay@movementstrategy.com>
2014-12-22 14:51:28 -0700

    made rows turn blue once they've been converted

--- client/views/draftBoardRow/draftBoardRow.js
+++ client/views/draftBoardRow/draftBoardRow.js
@@ -1,6 +1,12 @@
 Template['draftBoardRow'].helpers({
 	completed_class : function() {
-		return contentBucketHandler.rowIsCompleted(this) ? 'completed' : '';
+		
+		var converted = contentBucketHandler.bucketHasBeenConverted(this.content_bucket_id);
+		if(converted) {
+			return 'converted';
+		} else {
+			return contentBucketHandler.rowIsCompleted(this) ? 'completed' : '';
+		}
 	},
 	draft_variables : function() {
 		return contentBucketHandler.getDraftVariablesForRow(this);
