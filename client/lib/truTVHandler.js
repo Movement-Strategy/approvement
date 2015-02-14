35e45f81b97a9c7ed7d2b651efaffff91ca9a817
Clay Branch <clay@movementstrategy.com>
2015-02-14 08:54:24 -0700

    get required data structure for populating template processing

--- client/lib/truTVHandler.js
+++ client/lib/truTVHandler.js
@@ -33,6 +33,34 @@ truTVHandler = {
 			},
 		};	
 	},
+	getMetricMap : function() {
+		return {
+			approved : {
+				'check_item' : function(item) {
+					return item.scope == 'external' && item.status == 'approved';
+				},
+				color : 'green',
+			},
+			rejected : {
+				'check_item' : function(item) {
+					return item.scope == 'external' && item.status == 'rejected';
+				},
+				color : 'red',
+			},
+			submitted : {
+				'check_item' : function(item) {
+					return item.scope == 'internal';
+				},
+				color : 'grey',
+			},
+			in_pipeline : {
+				'check_item' : function(item) {
+					return item.scope == 'private';
+				},
+				color : 'purple',
+			},
+		};
+	},
 	getCellOptions : function() {
 		return _.map(this.getShowMap(), function(showDetails, showName){
 			return {
@@ -51,13 +79,86 @@ truTVHandler = {
 		});
 	},
 	getSummarizedShows : function() {
-		var approvalMetricsByShowByNetwork = this.getApprovalMetricsByShowByNetwork();		
+		var approvalMetricsByShowByNetwork = this.getApprovalMetricsByShowByNetwork();
+		return _.map(approvalMetricsByShowByNetwork, function(metricsByNetwork, showID){
+			var processedNetworks = truTVHandler.getProcessedNetworks(metricsByNetwork);
+			var profileImageURL = truTVHandler.getProfileImageURLForShowID(showID);
+			var showName = truTVHandler.getShowNameFromShowID(showID);
+			return {
+				show_id : showID,
+				name : showName,
+				image : profileImageURL,
+				networks : processedNetworks,
+			}
+		});
+	},
+	getIconMap : function() {
+		return {
+			facebook : 'facebook',
+			twitter : 'twitter',
+			linked : 'linkedin',
+			instagram : 'instagram',
+		};
+	},
+	getProcessedNetworks : function(metricsByNetwork){
+		// iterate over the icon map so we can make sure the networks are always in same order
+		return _.chain(this.getIconMap())
+			.map(function(icon, networkType){
+				if(_.has(metricsByNetwork, networkType)){
+					var processedMetrics = truTVHandler.getProcessedMetrics(metricsByNetwork[networkType]);
+					return {
+						icon : icon,
+						metrics : processedMetrics,
+					};
+				}
+			})
+			.compact()
+		.value();
+	},
+	getProcessedMetrics : function(metrics){
+		var metricMap = truTVHandler.getMetricMap();
+		return _.map(metrics, function(value, metricName){
+			var color = metricMap[metricName]['color'];
+			return {
+				icon_color : color,
+				value : value,
+			};
+		});
 	},
 	getApprovalMetricsByShowByNetwork : function() {
+		var approvalItems = this.getAllApprovalItemsFromCalendarDays();
+		var metricsByShowByNetwork = {};
+		_.map(approvalItems, function(approvalItem){
+			var networkType = approvalItem.type;
+			var showID = approvalItem.show;
+			if(!_.has(metricsByShowByNetwork, showID)) {
+				metricsByShowByNetwork[showID] = {};
+			}
+			if(!_.has(metricsByShowByNetwork[showID], networkType)) {
+				metricsByShowByNetwork[showID][networkType] = {};
+			}
+			_.map(truTVHandler.getMetricMap(), function(metricDetails, metricName){
+				if(!_.has(metricsByShowByNetwork[showID][networkType], metricName)) {
+					metricsByShowByNetwork[showID][networkType][metricName] = 0;
+				}
+				
+				if(metricDetails['check_item'](approvalItem)) {
+					metricsByShowByNetwork[showID][networkType][metricName] =  metricsByShowByNetwork[showID][networkType][metricName] + 1; 
+				}
+			});
+		});
 		
+		return metricsByShowByNetwork;
 	},
 	getAllApprovalItemsFromCalendarDays : function() {
-		
+		return _.chain(Session.get('calendar_days'))
+			.pluck('approval_items')
+			.compact()
+			.map(function(itemsForDay){
+				return _.flatten(itemsForDay);
+			})
+			.flatten()
+		.value();	
 	},
 	setShowInItem : function(item){
 		item['show'] = Session.get('current_show_id');
@@ -70,6 +171,10 @@ truTVHandler = {
 	},
 	getProfileImage : function(){
 		return this.getKeyForCurrentShow('profile');
+	},
+	getProfileImageURLForShowID : function(showID) {
+		var profileImage = this.getKeyFromMap('profile', showID);
+		return facebookHandler.getPictureURL(profileImage);		
 	},
 	getClientName : function(){
 		return this.getKeyForCurrentShow('name');
@@ -86,6 +191,9 @@ truTVHandler = {
 	},
 	getShowName : function(item) {
 		return this.getKeyFromItem('name', item);
+	},
+	getShowNameFromShowID : function(showID) {
+		return this.getKeyFromMap('name', showID);	
 	},
 	getKeyForCurrentShow : function(key){
 		return this.getKeyFromMap(key, Session.get('current_show_id'));
